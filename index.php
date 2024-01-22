5c2553af788a83e789f13dba582cd67c6b0a2fed
Ludmann Dorian <dorianludm7@gmail.com>
2024-01-22 16:10:23 +0100

    Remove useless code

--- index.php
+++ index.php
@@ -29,54 +29,6 @@ $questions = Factory::createQuestions($q);
 <?php
 
 
-function answer_text($q, $v) {
-    global $question_correct, $score_total, $score_correct;
-    $score_total += $q["score"];
-    if (empty($v)) return;
-    if ($q["answer"] == $v) {
-        $question_correct += 1;
-        $score_correct += $q["score"];
-        echo "<p> ✔️";
-    }else{
-        echo "<p> ❌";
-    }
-}
-
-function answer_radio($q, $v) {
-    global $question_correct, $score_total, $score_correct;
-    $score_total += $q["score"];
-    if (is_null($v)) return;
-    if ($q["answer"] == $v) {
-        $question_correct += 1;
-        $score_correct += $q["score"];
-        echo "<p> ✔️";
-    }else{
-        echo "<p> ❌";
-    }
-}
-
-
-function answer_checkbox($q, $v) {
-    global $question_correct, $score_total, $score_correct;
-    $score_total += $q["score"];
-    if (is_null($v)) return;
-    $diff1 = array_diff($q["answer"], $v);
-    $diff2 = array_diff($v, $q["answer"]);
-    if (count($diff1) == 0 && count($diff2) == 0) {
-        $question_correct += 1;
-        $score_correct += $q["score"];
-        echo "<p> ✔️";
-    }else{
-        echo "<p> ❌";
-    }
-}
-
-$answer_handlers = array(
-    "text" => "answer_text",
-    "radio" => "answer_radio",
-    "checkbox" => "answer_checkbox"
-);
-
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $question_number = 1;
     echo "<h1> Testez votre culture avec ce quiz! </h1>";
